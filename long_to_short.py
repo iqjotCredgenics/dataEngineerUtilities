b0c801ff36bb882057ff8809c2f4711b91d8f808
ketan-credgenics <106948045+ketan-credgenics@users.noreply.github.com>
2022-06-13 05:24:32 -0400

    Created new function
    Created function to convert long month name to short name

---
+++ long_to_short.py
@@ -0,0 +1,8 @@
+def long_to_short(long_month_name):
+#long_month_name = "January"
+    datetime_object = datetime.datetime.strptime(long_month_name, "%B")
+    month_number = datetime_object.month
+    month_number = str(month_number)
+    datetime_object = datetime.datetime.strptime(month_number, "%m")
+    month_name = datetime_object.strftime("%b")
+    return month_name
