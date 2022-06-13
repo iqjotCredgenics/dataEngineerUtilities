02218ba92b2c525c8d0bfca687d4502162b9905e
ketan-credgenics <106948045+ketan-credgenics@users.noreply.github.com>
2022-06-13 04:52:47 -0400

    Updating package file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='dataEngineerUtilities',
+    packages=find_packages(),
+    description='DB Library for Credgenics',
+    version='0.2.2',
+    url='',
+    author='Ketan Bassi',
+    author_email='ketan.bassi@credgenics.com',
+   
+)
