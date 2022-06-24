5f2a0b344f45fa98825908b2523bfbc74877b6a7
iqjotCredgenics <103040159+iqjotCredgenics@users.noreply.github.com>
2022-06-24 15:56:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
-import pkg_resources  # part of setuptools
 setup(
     name='dataEngineerUtilities',
     packages=['dataEngineerUtilities'],
     description='DB Library for Credgenics',
-    version= pkg_resources.require("dataEngineerUtilities")[0].version,
+    #version= pkg_resources.require("dataEngineerUtilities")[0].version,
     url='',
     author='Ketan Bassi',
     author_email='ketan.bassi@credgenics.com',
