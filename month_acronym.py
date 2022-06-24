d37a535fa2e960670625c7398a7f24ed91448f94
iqjotCredgenics <103040159+iqjotCredgenics@users.noreply.github.com>
2022-06-24 15:26:12 +0530

    Delete month_acronym.py

--- month_acronym.py
+++
@@ -1,8 +0,0 @@
-def month_acronym(long_month_name):
-#long_month_name = "January"
-    datetime_object = datetime.datetime.strptime(long_month_name, "%B")
-    month_number = datetime_object.month
-    month_number = str(month_number)
-    datetime_object = datetime.datetime.strptime(month_number, "%m")
-    month_name = datetime_object.strftime("%b")
-    return month_name
